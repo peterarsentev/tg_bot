7024dbd43a89c203cf3fd7cddaedf1328ed1a1bf
parsentev <parsentev@yandex.ru>
2023-12-15 15:49:26 +0300

    Wizard

--- src/main/java/ru/job4j/tg/Action.java
+++ src/main/java/ru/job4j/tg/Action.java
@@ -3,13 +3,6 @@ package ru.job4j.tg;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.job4j.tg.domain.Resp;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-
 public interface Action {
     Resp handle(Update update);
-
-    default Iterator<? extends Action> bindingActions() {
-       return new ArrayList<Action>().iterator();
-   }
 }
